ca081f60ecbc1d6a34846eaceff73e20c6c239e3
Riaz-RSA <109266957+RIAZ-143@users.noreply.github.com>
2022-09-26 09:27:04 +0500

    Update free.py

--- free.py
+++ free.py
@@ -4,18 +4,6 @@ print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
 
 os.system('git pull')
 
-try:
-
-    import gtts
-
-except IOError:
-
-    os.system('pip install gtts')
-
-def Update():
-
-    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
-
 
 
 
