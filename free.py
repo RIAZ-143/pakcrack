19caf861ac374fdb0f6d177f5ba213374238bed4
Riaz-RSA <109266957+RIAZ-143@users.noreply.github.com>
2022-09-26 09:25:33 +0500

    Update free.py

--- free.py
+++ free.py
@@ -1,5 +1,25 @@
 import pakcrack
 
+print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
+
+os.system('git pull')
+
+try:
+
+    import gtts
+
+except IOError:
+
+    os.system('pip install gtts')
+
+def Update():
+
+    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
+
+def Run():
+
+        bit = platform.architecture()[0]
+
 
 
 pakcrack.pak()
