6228c555d68747d5434c750208a3333f68d422e5
Riaz-RSA <109266957+RIAZ-143@users.noreply.github.com>
2022-09-26 09:24:24 +0500

    Update free.py

--- free.py
+++ free.py
@@ -1,27 +1,5 @@
 import pakcrack
 
-print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
 
-os.system('git pull')
-
-try:
-
-    import gtts
-
-except IOError:
-
-    os.system('pip install gtts')
-
-def Update():
-
-    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
-
-def Run():
-
-        bit = platform.architecture()[0]
-
-        if bit == '64bit':
-
-            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
 
 pakcrack.pak()
